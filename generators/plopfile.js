0c5cbc49aa5d68ac92bad7135affc2558c2bab1a
Augusto Lima <augustolimads@gmail.com>
2021-11-26 15:04:16 -0300

    configure plop

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -19,16 +19,28 @@ module.exports = (plop) => {
         path: "../src/components/{{pascalCase name}}/styles.ts",
         templateFile: "templates/styles.ts.hbs",
       },
-      {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/stories.tsx",
-        templateFile: "templates/stories.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "../src/components/{{pascalCase name}}/test.tsx",
-        templateFile: "templates/test.tsx.hbs",
-      },
     ],
-  });
+  }),
+    plop.setGenerator("template", {
+      description: "Create a template",
+      prompts: [
+        {
+          type: "input",
+          name: "name",
+          message: "What is your template name?",
+        },
+      ],
+      actions: [
+        {
+          type: "add",
+          path: "../src/templates/{{pascalCase name}}/index.tsx",
+          templateFile: "templates/Component.tsx.hbs",
+        },
+        {
+          type: "add",
+          path: "../src/templates/{{pascalCase name}}/styles.ts",
+          templateFile: "templates/styles.ts.hbs",
+        },
+      ],
+    });
 };
