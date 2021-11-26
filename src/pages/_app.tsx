5fc7ce6f6c7ec071d26c3bf14aeafd0642f9b515
Augusto Lima <augustolimads@gmail.com>
2021-11-26 15:28:30 -0300

    add fake server

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@material-ui/styles";
 import type { AppProps } from "next/app";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Head from "next/head";
 import theme from "src/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Head>
         <title>Boilerplate next material</title>
         <meta
