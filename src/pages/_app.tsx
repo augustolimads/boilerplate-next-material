5cc8ec5a712bd4b76cf951eb19412f081a8b3bcb
Augusto Lima <augustolimads@gmail.com>
2021-11-26 13:30:59 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "@material-ui/styles";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import theme from "src/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>Boilerplate next material</title>
+        <meta
+          name="description"
+          content="A simple project starter to work with typescript, React, NextJs, Redux Toolkit and Material UI"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core";
+
+export const Home = () => {
+  return (
+    <div>
+      <Typography variant="h1">Oi mundo</Typography>
+    </div>
+  );
+};
